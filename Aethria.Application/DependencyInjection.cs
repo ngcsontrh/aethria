2cac35ecfca14a906007a020c1d727ce3696e0f2
Ngoc Son <trhsonngoc12@gmail.com>
2026-05-29 15:56:23 +0700

    refactor(application): use DispatchR mcp pipeline registration

--- Aethria.Application/DependencyInjection.cs
+++ Aethria.Application/DependencyInjection.cs
@@ -29,46 +29,26 @@ public static class DependencyInjection
 
     public static IServiceCollection AddMcpApplicationServices(this IServiceCollection services)
     {
-        services.AddScoped<IMediator, Mediator>();
-
         services.AddScoped<IValidator<GetResourceSelectorQuery>, GetResourceSelectorQueryValidator>();
         services.AddScoped<IValidator<ResourceChatCommand>, ResourceChatCommandValidator>();
 
-        services.AddScoped<GetResourceSelectorQueryHandler>();
-        services.AddScoped<ResourceChatCommandHandler>();
-
-        services.AddScoped<IRequestHandler<GetResourceSelectorQuery, ValueTask<Result<GetResourceSelectorResponse>>>>(
-            sp => CreateValidatedHandler<GetResourceSelectorQuery, Result<GetResourceSelectorResponse>>(
-                sp,
-                sp.GetRequiredService<GetResourceSelectorQueryHandler>()));
-
-        services.AddScoped<IStreamRequestHandler<ResourceChatCommand, Result<ChatStreamResponse>>>(
-            sp => CreateValidatedStreamHandler<ResourceChatCommand, Result<ChatStreamResponse>>(
-                sp,
-                sp.GetRequiredService<ResourceChatCommandHandler>()));
+        services.AddDispatchR(options =>
+        {
+            options.Assemblies.Add(typeof(DependencyInjection).Assembly);
+            options.RegisterPipelines = true;
+            options.RegisterNotifications = false;
+            options.PipelineOrder =
+            [
+                typeof(ValidationResultPipelineBehavior<,>),
+                typeof(ValidationStreamPipelineBehavior<,>)
+            ];
+            options.IncludeHandlers =
+            [
+                typeof(GetResourceSelectorQueryHandler),
+                typeof(ResourceChatCommandHandler)
+            ];
+        });
 
         return services;
-    }
-
-    private static IRequestHandler<TRequest, ValueTask<TResponse>> CreateValidatedHandler<TRequest, TResponse>(
-        IServiceProvider serviceProvider,
-        IRequestHandler<TRequest, ValueTask<TResponse>> next)
-        where TRequest : class, IRequest<TRequest, ValueTask<TResponse>>
-        where TResponse : ResultBase<TResponse>, new()
-    {
-        var pipeline = ActivatorUtilities.CreateInstance<ValidationResultPipelineBehavior<TRequest, TResponse>>(serviceProvider);
-        pipeline.NextPipeline = next;
-        return pipeline;
-    }
-
-    private static IStreamRequestHandler<TRequest, TResponse> CreateValidatedStreamHandler<TRequest, TResponse>(
-        IServiceProvider serviceProvider,
-        IStreamRequestHandler<TRequest, TResponse> next)
-        where TRequest : class, IStreamRequest<TRequest, TResponse>
-        where TResponse : ResultBase<TResponse>, new()
-    {
-        var pipeline = ActivatorUtilities.CreateInstance<ValidationStreamPipelineBehavior<TRequest, TResponse>>(serviceProvider);
-        pipeline.NextPipeline = next;
-        return pipeline;
     }
 }
